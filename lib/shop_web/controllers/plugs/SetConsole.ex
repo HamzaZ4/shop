f3e6d0ea426b7464d1952767420d5b2a73e04e21
hamzakhalfi <mchamz72@gmail.com>
2025-09-18 11:23:27 -0400

    comit

---
+++ lib/shop_web/controllers/plugs/SetConsole.ex
@@ -0,0 +1,25 @@
+defmodule ShopWeb.SetConsole do
+  @valid_consoles = ["nintendo", "playstation", "pc", "xbox"]
+  @thirty_days_in_code = 2_592_000
+
+  def init(default_console), do: default_console
+
+  def call(%Plug.Conn{params: %{"console" => console}} = conn, _default_console)
+      when console in @valid_consoles do
+    conn
+    |> assign(:console, console)
+    |> put_resp_cookie("console", console, max_age: @thirty_days_in_code)
+  end
+
+  def call(%Plug.Conn{cookies: %{"console" => console}}, _default_console)
+      when console in @default_console do
+    conn
+    |> assign(:console, console)
+  end
+
+  def call(%Plug.Conn{} = conn, default_console) do
+    conn
+    |> assign(:console, default_console)
+    |> put_resp_cookie("console", console, max_age: @thirty_days_in_code)
+  end
+end
